eab3e5b8e6c3de18f4905e4015e7e9f14f23f3d6
arie-rodzi <zahari@uitm.edu.my>
2025-06-14 06:36:08 +0800

    Add files via upload

---
+++ login_penyelaras.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("🔐 Login")
+
+with st.form("login_form"):
+    nama = st.text_input("Nama")
+    kata_laluan = st.text_input("Kata Laluan", type="password")
+    submitted = st.form_submit_button("Login")
+    if submitted:
+        if nama and kata_laluan:
+            st.session_state["penyelaras"] = ("id", "nama")
+            st.success("Login berjaya!")
+            st.rerun()
+        else:
+            st.warning("Sila masukkan nama dan kata laluan.")
